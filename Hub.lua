5a4b07c89248f1df974188c8395b1e96cc014d65
Coolmesir <93215371+Coolmesir@users.noreply.github.com>
2022-04-03 19:01:02 +0800

    Create Hub.lua

---
+++ Hub.lua
@@ -0,0 +1,57 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Test", "Sentinel")
+
+local Tab = Window:NewTab("Main")
+local MainSection = Tab:NewSection("Main")
+MainSection:NewSlider("WalkSpeed", "More Speed", 500, 0, function(s)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+MainSection:NewSlider("JumpPower", "More JumpPower", 500, 0, function(s) game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+
+
+
+local Tab = Window:NewTab("Scripts")
+local ScriptsSection = Tab:NewSection("Scripts")
+ScriptsSection:NewDropdown("Scripts", "DropdownInf", {"Fly Gui", "Esp Pc Only", "Esp For Mobile/Pc"}, function(s)
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Coolmesir/My-Own-Script-Roblox/main/Flygui.lua"))()
+
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/kvr0AuWz"))()
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Coolmesir/My-Own-Script-Roblox/main/Esp.lua"))()
+    print(s)
+end)
+
+
+
+
+local ScriptsSection = Tab:NewSection("Games")
+ScriptsSection:NewDropdown("Prison Life", "DropdownInf", {"M9", "Remington 870", "Admin"}, function(s)
+local A_1 = game:GetService("Workspace")["Prison_ITEMS"].giver.M9.ITEMPICKUP
+local Event = game:GetService("Workspace").Remote.ItemHandler
+Event:InvokeServer(A_1)
+
+local A_1 = game:GetService("Workspace")["Prison_ITEMS"].giver["Remington 870"].ITEMPICKUP
+local Event = game:GetService("Workspace").Remote.ItemHandler
+Event:InvokeServer(A_1)
+
+local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/XTheMasterX/Scripts/Main/PrisonLife'),true))()
+    print(s)
+end)
+
+local Tab = Window:NewTab("Keybinds")
+local KeybindsSection = Tab:NewSection("Keybinds")
+KeybindsSection:NewKeybind("Soon?!", "Soon", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
+
+
+
+
+local Tab = Window:NewTab("Guis")
+local GuisSection = Tab:NewSection("Guis")
+GuisSection:NewButton("Troll Gui", "Trolling Gui", function()
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/RNWgaqCE", true))()
+    print("Clicked")
+end)

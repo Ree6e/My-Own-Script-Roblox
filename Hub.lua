5fe3eab4533e2340b3f96da38ae1d110b0f41918
Coolmesir <93215371+Coolmesir@users.noreply.github.com>
2022-04-04 01:44:25 +0800

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -9,6 +9,8 @@ end)
 MainSection:NewSlider("JumpPower", "More JumpPower", 500, 0, function(s) game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 end)
 
+
+
 local Tab = Window:NewTab("Scripts")
 local ScriptsSection = Tab:NewSection("Scripts")
 ScriptsSection:NewDropdown("Scripts", "DropdownInf", {"Fly Gui", "Esp Pc Only", "Esp For Mobile/Pc"}, function(s)
@@ -19,6 +21,9 @@ local Library = loadstring(game:HttpGet("https://pastebin.com/raw/kvr0AuWz"))()
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Coolmesir/My-Own-Script-Roblox/main/Esp.lua"))()
     print(currentOption)
 end)
+
+
+
 
 local ScriptsSection = Tab:NewSection("Games")
 ScriptsSection:NewDropdown("Prison Life", "DropdownInf", {"M9", "Remington 870", "Admin"}, function(s)
